d1910dc48370683d57a05a613a6e310ba090348b
lpanebianco <114702071+lpanebianco@users.noreply.github.com>
2025-07-07 14:26:30 +0200

    Add setup.py for package configuration

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gdeltngrams",
+    version="1.0.0",
+    description='Python package for accessing the GDELT Web News NGrams 3.0 API and reconstructing full-text news articles.',
+    author="Lorenzo Panebianco",
+    license="GPL-3.0-or-later",  # SPDX identifier
+    packages=find_packages(),
+    install_requires=[
+        "pandas",
+        "requests",
+        "tqdm"
+    ],
+    python_requires='>=3.7',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+)
